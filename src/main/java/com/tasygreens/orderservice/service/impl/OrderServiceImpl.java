4b25d981cdfdba768c3403877e872cfc60e7fd7f
Bagylakshmi, Rithick (Contractor) <Rithick_Bagylakshmi@comcast.com>
2022-08-16 01:12:59 +0530

    Removed unused imports

--- src/main/java/com/tasygreens/orderservice/service/impl/OrderServiceImpl.java
+++ src/main/java/com/tasygreens/orderservice/service/impl/OrderServiceImpl.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.Optional;
 
